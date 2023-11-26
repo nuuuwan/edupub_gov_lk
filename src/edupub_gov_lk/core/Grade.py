b5bc01e4fae00fb181d2904016760d47dd3001ce
nuuuwan <nuuuwan@gmail.com>
2023-11-26 12:28:52 +0530

    All grades and langs

--- src/edupub_gov_lk/core/Grade.py
+++ src/edupub_gov_lk/core/Grade.py
@@ -13,18 +13,18 @@ class Grade:
     @staticmethod
     def list() -> list['Grade']:
         return [
-            # Grade(1, "Grade 1"),
-            # Grade(2, "Grade 2"),
-            # Grade(3, "Grade 3"),
-            # Grade(4, "Grade 4"),
-            # Grade(5, "Grade 5"),
-            # Grade(6, "Grade 6"),
-            # Grade(7, "Grade 7"),
-            # Grade(8, "Grade 8"),
+            Grade(1, "Grade 1"),
+            Grade(2, "Grade 2"),
+            Grade(3, "Grade 3"),
+            Grade(4, "Grade 4"),
+            Grade(5, "Grade 5"),
+            Grade(6, "Grade 6"),
+            Grade(7, "Grade 7"),
+            Grade(8, "Grade 8"),
             Grade(9, "Grade 9"),
             Grade(10, "Grade 10"),
             Grade(11, "Grade 11"),
-            # Grade(17, "Grade 12/13"),
+            Grade(17, "Grade 12/13"),
         ]
 
     @staticmethod
